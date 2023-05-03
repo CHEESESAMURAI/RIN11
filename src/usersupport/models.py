c72a2f3a642d821f2d988fba0184a567a91ab38b
CHEESESAMURAI <84150366+CHEESESAMURAI@users.noreply.github.com>
2023-05-03 10:11:58 +0300

    Update models.py

--- src/usersupport/models.py
+++ src/usersupport/models.py
@@ -23,6 +23,8 @@ class TelegramUser(TimeBasedModel):
     second_name = models.CharField(max_length=255, verbose_name="SirName", default="")
     username = models.CharField(max_length=255, verbose_name="username", default="")
     user_role = models.CharField(max_length=255, verbose_name="Роль")
+    user_ref = models.CharField(max_length=255, verbose_name="Реферал")
+    ref_col = models.CharField(max_length=255, verbose_name="Кол-во реф")
     chat_id = models.BigIntegerField(verbose_name="Чат пользователя", default=0)
     chanel_id = models.BigIntegerField(verbose_name="Канал пользователя", default=0)
 
