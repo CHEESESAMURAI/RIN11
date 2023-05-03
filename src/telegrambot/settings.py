4b656bad51a3526a0c960fe9b58fe5905b2968ed
CHEESESAMURAI <84150366+CHEESESAMURAI@users.noreply.github.com>
2023-05-03 10:04:07 +0300

    Update settings.py

--- src/telegrambot/settings.py
+++ src/telegrambot/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "telegrambot.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": 'RIN1',
+        "NAME": 'GIN19',
         "USER": 'usupportuser',
         "PASSWORD": 'cheese_901',
         "HOST": 'localhost',
