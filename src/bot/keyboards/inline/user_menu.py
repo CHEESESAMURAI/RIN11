2094ee2922ea0d4ba31ccf2e08bdd0a10d7f03a1
CHEESESAMURAI <84150366+CHEESESAMURAI@users.noreply.github.com>
2023-08-28 13:56:38 +0300

    Update user_menu.py

--- src/bot/keyboards/inline/user_menu.py
+++ src/bot/keyboards/inline/user_menu.py
@@ -5,16 +5,108 @@ from bot.data import keyboards_data as kd
 __all__ = [
     "get_user_menu",
     "back",
+    "course",
+    "reg_ref",
+    "instruction",
+    "fio",
+    "trade",
+    "take",
+    "take_fio",
+    "take_num",
+    "scren",
+    "final",
 ]
 
 
 async def get_user_menu():
-    keyboard = InlineKeyboardMarkup()
-
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(
+        InlineKeyboardButton("🎓 О проекте",callback_data="act_1"),
+        InlineKeyboardButton("😉 Приведи друга",callback_data="act_2"),
+        InlineKeyboardButton("💻Социальные сети",callback_data="act_15"),
+        InlineKeyboardButton("✔ Зарегистрироваться на курс",callback_data="act_3")
+    )
+    return keyboard
+async def scren():
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(
+        InlineKeyboardButton(text="🔙 Назад",callback_data="back"),
+        InlineKeyboardButton(text="📷 Загрузить скриншот",callback_data="act_14"),
+        InlineKeyboardButton("✔ Зарегистрироваться на курс",callback_data="act_3")
+    )
     return keyboard
 
-
 async def back():
-    keyboard = InlineKeyboardMarkup()
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(InlineKeyboardButton(text="🔙 Назад",callback_data="back"),
+    InlineKeyboardButton("✔ Зарегистрироваться на курс",callback_data="act_3")
+    )
+    return keyboard
 
+async def final():
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(
+        InlineKeyboardButton(text="🔙 Назад",callback_data="back"),
+        InlineKeyboardButton(text="✔ Зарегистрироваться на Госуслуги",url="https://www.gosuslugi.ru/futurecode?organization=44&view=online"),
+        InlineKeyboardButton(text="📷 Загрузить скриншот",callback_data="act_14"),
+    )
+    return keyboard
+
+async def course():
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(
+        InlineKeyboardButton("🤖 Telegram-боты на Python",callback_data="act_4"),
+        InlineKeyboardButton("🎮 Разработка 2d-игр на Python",callback_data="act_5"),
+        InlineKeyboardButton(text="🔙 Назад ",callback_data="back"),
+     )
+    return keyboard
+
+async def reg_ref():
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(
+        InlineKeyboardButton("🔗 Реферальное приглашение",callback_data="act_6"),
+        InlineKeyboardButton("🎁 Обменять баллы",callback_data="act_9"),
+        InlineKeyboardButton("📝 Список приглашенных",callback_data="act_10"),
+        InlineKeyboardButton("✔ Зарегистрироваться на курс",callback_data="act_3"),
+    )
+    return keyboard
+
+async def take_fio():
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(
+        InlineKeyboardButton("✅  Я написал свое ФИО",callback_data="act_12"),
+    )
+    return keyboard
+
+async def take_num():
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(
+        InlineKeyboardButton("✅  Я написал свой номер телефона",callback_data="act_13"),
+    )
+    return keyboard
+
+async def instruction():
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(
+        InlineKeyboardButton("✅  Я написал свою почту",callback_data="act_8"),
+    )
+    return keyboard
+async def trade():
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(
+        InlineKeyboardButton("🪙 Получить сертификат",callback_data="act_11"),
+    )
+    return keyboard
+async def take():
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(
+        InlineKeyboardButton("✅  Я написал все данные!",callback_data="back"),
+    )
+    return keyboard
+async def fio():
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(
+        InlineKeyboardButton(text="🔙 Назад ",callback_data="act_3"),
+        InlineKeyboardButton("🫡 Выбираю этот курс",callback_data="act_7"),
+    )
     return keyboard
