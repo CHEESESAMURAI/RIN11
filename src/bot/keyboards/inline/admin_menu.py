d39e18ecbe2ff14bf5b7221021e0caf2a6140468
CHEESESAMURAI <84150366+CHEESESAMURAI@users.noreply.github.com>
2023-08-28 13:57:38 +0300

    Update admin_menu.py

--- src/bot/keyboards/inline/admin_menu.py
+++ src/bot/keyboards/inline/admin_menu.py
@@ -7,5 +7,9 @@ __all__ = [
 
 async def get_admin_menu(user_id):
     keyboard = InlineKeyboardMarkup()
-
+    keyboard.add(
+        InlineKeyboardButton(text='ok',callback_data=f"code_{user_id}"),
+        InlineKeyboardButton(text='not',callback_data=f"denied_{user_id}"),
+    )
     return keyboard
+~                                
