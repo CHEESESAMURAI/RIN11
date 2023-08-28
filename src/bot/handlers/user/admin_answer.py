b84416e16522888e87eced8271ff0dfe12a6466a
CHEESESAMURAI <84150366+CHEESESAMURAI@users.noreply.github.com>
2023-08-28 13:50:38 +0300

    Update admin_answer.py

--- src/bot/handlers/user/admin_answer.py
+++ src/bot/handlers/user/admin_answer.py
@@ -1,19 +1,23 @@
 from aiogram import types
 
+
 from bot.config.loader import bot
 from usersupport.models import TelegramUser, UserDiscussion
 from bot.services.db import user as user_db
 from bot.services.db import user_discussion as ud_db
 from bot.data import text_data as td
-
+from bot.handlers.user import create_user_disck as reeef
+from bot.keyboards import inline as ik
 
 async def answer_from_admin(message: types.Message):
+
     user_id = int(message.reply_to_message.text.split("\n")[0])
     user: TelegramUser = await user_db.select_user(
         user_id=user_id
     )
     d: UserDiscussion = await ud_db.select_discussion(user=user)
     answer = message.text
+    klava = await ik.reg_ref()
     history = f"{d.history}\nA:{answer}"
     await ud_db.add_history(user=user, history=history)
     await bot.send_message(
@@ -24,18 +28,37 @@ async def answer_from_admin(message: types.Message):
 
 
 async def send_code(call: types.CallbackQuery):
-    user_id = call.data.replace("code_", "")
-    await send_mes(user_id, td.TEXT_ANSWER_2)
+    global ref_col
+    global ref_coll
+    global klava
+
+    user_id = call.data.replace("code_", "",)
+    await bot.send_message(user_id,"ПОЗДРАВЛЯЮ 🎉🎉🎉\n\nТы успешно сдал тест и прошел регистрацию!\n\nВпереди увлекательное лето с Иннополис!\n\n🪙 Теперь на твоём бонусном счёте прибавились 50 баллов!\n\nНе останавливайся на достигнутом!\n\nПриглашай на интенсив одноклассников и друзей, копи Бадди баллы, чтобы потом получить за них подарки 🎁", reply_markup = await ik.reg_ref())
     await call.answer()
-    
-    #user_ref: TelegramUser = await user_db.select_user(
-    #    user_ref=user_ref
-    #)
-   # ref_col
-    
+
     user: TelegramUser = await user_db.select_user(
         user_id=user_id
     )
+
+    ref_id=TelegramUser.objects.filter(user_id=user_id).first().user_ref
+    print(ref_id)
+    user_ref_coll = TelegramUser.objects.filter(user_id=ref_id).first().ref_col
+
+    new_ref_col = int(user_ref_coll) + 1
+
+    TelegramUser.objects.filter(user_id=ref_id).update(ref_col=new_ref_col)
+
+    user_ref_coll = TelegramUser.objects.filter(user_id=user_id).first().ref_col
+
+    new_ref_col = int(user_ref_coll) + 1
+
+    TelegramUser.objects.filter(user_id=user_id).update(ref_col=new_ref_col)
+    #print(reeef.reef)
+    #print(reeef.reef.title)
+    #print(str(reeef.reef))
+
+    #reeef.reef()
+    
     d: UserDiscussion = await ud_db.select_discussion(user=user)
     admins = await user_db.select_all_admins()
     a_list = {a.chanel_id: a.chat_id for a in admins}
@@ -44,7 +67,8 @@ async def send_code(call: types.CallbackQuery):
         await bot.send_message(
             chat_id=a_list[admin],
             text=td.SUCCESS_MESSAGE_CARD,
-            reply_to_message_id=mes_id[a_list[admin]]
+            reply_to_message_id=mes_id[a_list[admin]],
+
         )
 
 
