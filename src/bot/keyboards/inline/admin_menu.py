db354d71897caae414aa78ccb7e5cccb730e8614
CHEESESAMURAI <84150366+CHEESESAMURAI@users.noreply.github.com>
2023-09-20 10:24:00 +0300

    Update admin_menu.py

--- src/bot/keyboards/inline/admin_menu.py
+++ src/bot/keyboards/inline/admin_menu.py
@@ -1,4 +1,4 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+    from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 __all__ = [
     "get_admin_menu"
@@ -8,8 +8,10 @@ __all__ = [
 async def get_admin_menu(user_id):
     keyboard = InlineKeyboardMarkup()
     keyboard.add(
-        InlineKeyboardButton(text='ok',callback_data=f"code_{user_id}"),
-        InlineKeyboardButton(text='not',callback_data=f"denied_{user_id}"),
+        InlineKeyboardButton(text='Скрин да',callback_data=f"code_{user_id}"),
+        InlineKeyboardButton(text='Скрин нет',callback_data=f"denied_{user_id}"),
+        InlineKeyboardButton(text='Промо да',callback_data=f"code2_{user_id}"),
+        InlineKeyboardButton(text='Промо нет',callback_data=f"denied2_{user_id}"),
+        InlineKeyboardButton(text='+50',callback_data=f"plus_{user_id}")
     )
     return keyboard
-~                                
